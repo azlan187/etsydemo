3e6f74bbd761b4b27cdd95eb7f561210d28b414c
azlan187 <azlan9190@gmail.com>
2014-04-02 17:31:34 +0800

    Add default.jp to listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,7 @@ class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :default_url => "default.jpg",:styles => { :medium => "200x", :thumb => "100x100>" }
 	else
-		has_attached_file :image, :default_url => "default.jpng", 
+		has_attached_file :image, :default_url => "default.jpg", 
 						  :storage => :dropbox,
 	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	    				  :path => ":style/:id_:filename"
