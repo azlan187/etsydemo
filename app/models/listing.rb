00017d070d413daa9c64f2b02a5f8f3ac941d0ad
azlan187 <azlan9190@gmail.com>
2014-04-02 15:02:07 +0800

    change listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,11 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-		has_attached_file :image, :default_url => "default.jpg"
-	else
-		has_attached_file :image, :default_url => "default.jpng", 
-						  :storage => :dropbox,
-	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-	    				  :path => ":style/:id_:filename"
-	end
+	has_attached_file :image, :default_url => "default.jpg"
+	# if Rails.env.development?
+	# 	has_attached_file :image, :default_url => "default.jpg"
+	# else
+	# 	has_attached_file :image, :default_url => "default.jpng", 
+	# 					  :storage => :dropbox,
+	#     				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	#     				  :path => ":style/:id_:filename"
+	# end
 end
