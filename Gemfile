cdec7f1f0c7b84c45287a799585b2c1c56893045
azlan187 <azlan9190@gmail.com>
2014-04-02 14:48:11 +0800

    Downgrade paperclip to version3.0, remove style from listing.rb and solve image uploading problem

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'coffee-rails', '~> 4.0.0' # Use CoffeeScript for .js.coffee assets and view
 gem 'jquery-rails' # Use jquery as the JavaScript library
 gem 'turbolinks' # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'jbuilder', '~> 1.2' # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem "paperclip", "~> 4.1"
+gem "paperclip", "~> 3.0"
 gem "paperclip-dropbox", ">= 1.1.7"
 gem "figaro"
  
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,10 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
+	if Rails.env.development?
+		has_attached_file :image, :default_url => "default.jpg"
 	else
-		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png", 
+		has_attached_file :image, :default_url => "default.jpng", 
 						  :storage => :dropbox,
-	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-
-		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-	    # validates_attachment_content_type :image, :content_type =>  /\Aimage\/.*\Z/
+	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	    				  :path => ":style/:id_:filename"
 	end
 end
-
