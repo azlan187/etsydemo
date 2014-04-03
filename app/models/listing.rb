35d3eba86381d5ecb2f8a4c56c173d366a96722b
azlan187 <azlan9190@gmail.com>
2014-04-03 15:17:48 +0800

    Add validations for Users and Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,4 @@
 class Listing < ActiveRecord::Base
-	# has_attached_file :image,
- #      				  :default_url => "default.jpg",
- #      				  :storage => :dropbox,
- #      				  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
- #      				  # :styles => { :medium => "200x", :thumb => "100x100>" },
 	if Rails.env.development?
 		has_attached_file :image, :default_url => "default.jpg",:styles => { :medium => "200x", :thumb => "100x100>" }
 	else
@@ -12,4 +7,8 @@ class Listing < ActiveRecord::Base
 	    				  :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	    				  :path => ":style/:id_:filename"
 	end
+
+	validates :name, :description, :price, presence: true # validates that the field is not blank
+	validates :price, numericality: { greater_than: 0} # validates that the price is number and bigger than 0
+	validates_attachment_presence :image
 end
